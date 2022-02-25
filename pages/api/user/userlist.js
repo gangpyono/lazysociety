f058b37dbcbefcc2659926e2a8caf532a0f8b206
shrkdvy <shrkdvy123@gmail.com>
2022-02-25 14:50:12 +0900

    [feat] : server userlist api

---
+++ pages/api/user/userlist.js
@@ -0,0 +1,25 @@
+import { readDB } from "../../../lib/dbController.js";
+
+const getUserList = () => readDB("userList");
+
+export default async function handle(req, res) {
+  const { method } = req;
+
+  try {
+    switch (method) {
+      case "GET":
+        const userList = await getUserList();
+        res.status(200).json({
+          userList,
+        });
+        break;
+      default:
+        res.setHeader("Allow", ["GET"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } catch (error) {
+    res.status(500).json({
+      msg: "예상치못한 오류 발생.",
+    });
+  }
+}
